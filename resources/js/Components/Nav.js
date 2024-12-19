eed3d0668ff94f8bca127dfa1f7059db46da0a3f
zikri-muhammad <muhammadzikri598@gmail.com>
2024-12-19 17:32:29 +0700

    nav && app

---
+++ resources/js/Components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+    return;
+    <>
+        <div className="w-full bg-red-600">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+            </ul>
+        </div>
+    </>;
+}
+
+export default Nav;
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,9 @@ require("./bootstrap");
 import React from "react";
 import ReactDOM from "react-dom/client";  // Mengimpor dari 'react-dom/client' untuk React 18
 import App from "./Components/App";
+import Nav from "./Components/Nav";
 
 if (document.getElementById("app")) {
     const root = ReactDOM.createRoot(document.getElementById("app"));
-    root.render(<App />);
+    root.render(<Nav />);
 }
